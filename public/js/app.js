c13429cdbaa02ba557c2fab6c583026aeba4ca4e
ShaQayeQ <shaghay3gh@gmail.com>
2025-02-04 21:45:19 +0100

    buggy working code

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,25 @@
 console.log("this is coming from app.js file!");
 
-fetch("http://localhost:3000/weather?city=")
-	.then((response) => {
-		if (!response.ok) {
-			throw new Error("HTTP error status: " + response.status);
-		}
-		return response.json();
-	})
-	.then((data) => {
-		if (data.error) {
-			throw new Error(data.error);
-		}
+const weatherForm = document.querySelector("form");
+const cityName = weatherForm.querySelector("#city");
 
-		console.log("location: " + data.location);
-		console.log("forcast: " + data.forcast);
-	})
-	.catch((error) => console.log(error));
+weatherForm.addEventListener("submit", (event) => {
+	// prevent the eventListinner to rerender*refresh) the page
+	event.preventDefault();
+	fetch("http://localhost:3000/weather?city=" + cityName.value)
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("HTTP error status: " + response.status);
+			}
+			return response.json();
+		})
+		.then((data) => {
+			if (data.error) {
+				throw new Error(data.error);
+			}
+
+			console.log("location: " + data.location);
+			console.log("forcast: " + data.forcast);
+		})
+		.catch((error) => console.log(error));
+});
