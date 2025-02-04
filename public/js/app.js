2b8d6061e2a2dda0ba6f8d6ab6c29e49d0dc50eb
ShaQayeQ <shaghay3gh@gmail.com>
2025-02-04 19:49:08 +0100

    fetch data from web-app api

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,32 @@
 console.log("this is coming from app.js file!");
 
-fetch("https://puzzle.mead.io/puzzle")
+// fetch("https://puzzle.mead.io/puzzle")
+// 	.then((response) => {
+// 		// when if blocks runs then the error will be thrown and the rest of the program won't be executes
+// 		if (!response.ok) {
+// 			throw new Error("HTTP error status: " + response.status);
+// 		}
+// 		// response.json() is a prommise => it should be resolved (in then block)
+// 		return response.json();
+// 	})
+// 	.then((data) => {
+// 		console.log("data: " + data.puzzle);
+// 	})
+// 	.catch((error) => console.log(error));
+
+fetch("http://localhost:3000/weather?city=Leipzi")
 	.then((response) => {
-		// when if blocks runs then the error will be thrown and the rest of the program won't be executes
 		if (!response.ok) {
 			throw new Error("HTTP error status: " + response.status);
 		}
-		// response.json() is a prommise => it should be resolved (in then block)
 		return response.json();
 	})
 	.then((data) => {
-		console.log("data: " + data.puzzle);
+		if (data.error) {
+			throw new Error(data.error);
+		}
+
+		console.log("location: " + data.location);
+		console.log("forcast: " + data.forcast);
 	})
 	.catch((error) => console.log(error));
-
-// fetch("http://localhost:3000/weather?city=Leipzig").then((response) => {
-// 	if (!response.ok) {
-// 		throw new Error("HTTP error status: " + response.status);
-// 	}
-// });
