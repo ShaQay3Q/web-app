c3ed7937d10a6a11497af74050708c5b34e8b475
ShaQayeQ <shaghay3gh@gmail.com>
2025-02-04 19:17:17 +0100

    try to handle errors with catch

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,21 @@
 console.log("this is coming from app.js file!");
 
-fetch("https://puzzle.mead.io/puzzle").then((response) => {
-	if (!response.ok) {
-		throw new Error("HTTP error status: " + response.status);
-	}
-	// response.jason() is a prommise => it should be resolved
-	response.json().then((data) => {
+fetch("https://puzzle.mead.io/puzzle")
+	.then((response) => {
+		// when if blocks runs then the error will be thrown and the rest of the program won't be executes
+		if (!response.ok) {
+			throw new Error("HTTP error status: " + response.status);
+		}
+		// response.json() is a prommise => it should be resolved (in then block)
+		return response.json();
+	})
+	.then((data) => {
 		console.log("data: " + data.puzzle);
-		return data.puzzle;
-	});
-});
+	})
+	.catch((error) => console.log(error));
+
+// fetch("http://localhost:3000/weather?city=Leipzig").then((response) => {
+// 	if (!response.ok) {
+// 		throw new Error("HTTP error status: " + response.status);
+// 	}
+// });
