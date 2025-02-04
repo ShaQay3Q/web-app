073d009c31aae08de9648d98482df4cc67892d70
ShaQayeQ <shaghay3gh@gmail.com>
2025-02-04 18:55:45 +0100

    fetch JSON data from a URL parse it into JS Object

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
 console.log("this is coming from app.js file!");
+
+fetch("https://puzzle.mead.io/puzzle").then((response) => {
+	if (!response.ok) {
+		throw new Error("HTTP error status: " + response.status);
+	}
+	// response.jason() is a prommise => it should be resolved
+	response.json().then((data) => {
+		console.log("data: " + data.puzzle);
+		return data.puzzle;
+	});
+});
